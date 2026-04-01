a0c5a51838f0e64ab272bb399ccdc1f78e76540d
teo-garcia <juanmateogarcia96@gmail.com>
2026-04-01 15:33:02 -0500

    refactor: adopt defineConfig syntax

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,13 @@
 import base from '@teo-garcia/eslint-config-shared/base'
+import { defineConfig } from 'eslint/config'
 import sharedPlaywright from '@teo-garcia/eslint-config-shared/playwright'
 import sharedReact from '@teo-garcia/eslint-config-shared/react'
 import astroPlugin from 'eslint-plugin-astro'
 
-export default [
+export default defineConfig([
   { ignores: ['.astro/**'] },
   ...base,
   ...sharedReact,
   ...astroPlugin.configs.recommended,
   ...sharedPlaywright,
-]
+])
