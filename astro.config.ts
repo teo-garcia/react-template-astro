f38bb1a431c5558d182394efb5bd9b7b789004c4
teo-garcia <juanmateogarcia96@gmail.com>
2026-05-01 09:21:59 -0500

    feat: add bundle analysis tooling

--- astro.config.ts
+++ astro.config.ts
@@ -2,8 +2,10 @@ import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import vercel from '@astrojs/vercel'
 import { defineConfig } from 'astro/config'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 const isVercel = process.env.VERCEL === '1'
+const shouldAnalyze = process.env.ANALYZE === 'true'
 
 export default defineConfig({
   site: process.env.PUBLIC_SITE_URL ?? 'http://localhost:4321',
@@ -14,6 +16,15 @@ export default defineConfig({
     port: 4321,
   },
   vite: {
+    plugins: [
+      shouldAnalyze &&
+        visualizer({
+          brotliSize: true,
+          filename: 'dist/analyze/stats.html',
+          gzipSize: true,
+          open: false,
+        }),
+    ],
     server: {
       watch: {
         usePolling: true,
